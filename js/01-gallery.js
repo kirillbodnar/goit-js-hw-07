783a2b8c5ebc387a03c9a7ba39f5adc362fd8c09
kirillbodnar <91017699+kirillbodnar@users.noreply.github.com>
2022-04-04 19:47:11 +0300

    Event listener fix
    Script now removes Event listener before closing the gallery item

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,13 +1,13 @@
-import { galleryItems } from "./gallery-items.js";
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const gallery = document.querySelector(".gallery");
+const gallery = document.querySelector('.gallery');
 
 const itemsMarkUp = createGalleryElementMarkup(galleryItems);
 
-gallery.insertAdjacentHTML("beforeend", itemsMarkUp);
+gallery.insertAdjacentHTML('beforeend', itemsMarkUp);
 
-gallery.addEventListener("click", onGalleryElementClick);
+gallery.addEventListener('click', onGalleryElementClick);
 
 function createGalleryElementMarkup(items) {
   return items
@@ -24,25 +24,30 @@ function createGalleryElementMarkup(items) {
     </a>
   </div>`;
     })
-    .join("");
+    .join('');
 }
 
 function onGalleryElementClick(event) {
   event.preventDefault();
-  if (event.target.nodeName !== "IMG") {
+  if (event.target.nodeName !== 'IMG') {
     return;
   }
 
   const FullScreenImgUrl = `<img src=${event.target.dataset.source} alt=${event.target.alt}>`;
 
-  const instance = basicLightbox.create(FullScreenImgUrl);
+  const instance = basicLightbox.create(FullScreenImgUrl, {
+    onShow: () => {
+      window.addEventListener('keydown', onKeyboardCloseModal);
+    },
+    onClose: () => {
+      window.removeEventListener('keydown', onKeyboardCloseModal);
+    },
+  });
 
   instance.show();
 
-  window.addEventListener("keydown", onKeyboardCloseModal);
-
   function onKeyboardCloseModal(event) {
-    if (event.code === "Escape" && instance.visible()) {
+    if (event.code === 'Escape' && instance.visible()) {
       instance.close();
     }
   }
